434e1270b7de2f8ceef6684558b63655bc8d37a2
ifty64bit <ifty64bit@gmail.com>
2025-12-24 15:52:51 +0600

    chore: Update Firebase SDK to v10.7.0 and replace dynamic config with hardcoded values.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,26 @@
 // Firebase Messaging Service Worker
 // This handles background push notifications when the app is not in focus
 
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
-// Initialize Firebase in the service worker
-// Note: The config will be passed when initializing messaging
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-  apiKey: self.FIREBASE_CONFIG?.apiKey || '',
-  authDomain: self.FIREBASE_CONFIG?.authDomain || '',
-  projectId: self.FIREBASE_CONFIG?.projectId || '',
-  storageBucket: self.FIREBASE_CONFIG?.storageBucket || '',
-  messagingSenderId: self.FIREBASE_CONFIG?.messagingSenderId || '',
-  appId: self.FIREBASE_CONFIG?.appId || '',
+  apiKey: "AIzaSyBtYuZH7hfXA6SM4O41r-IiDyJ5hgTOdaI",
+  authDomain: "threads-of-shayari-ad46a.firebaseapp.com",
+  projectId: "threads-of-shayari-ad46a",
+  storageBucket: "threads-of-shayari-ad46a.appspot.com",
+  messagingSenderId: "775383540594",
+  appId: "1:775383540594:web:dfe562ac7cff153869b3eb",
 });
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
 // Handle background messages
